b7a42de82d99741ee3236a3e97bea31e9cdfe72b
Anton Chernyshev <a.dm.chernyshev@gmail.com>
2019-06-19 20:31:32 +0300

    made architecture edits

---
+++ DeckBuilder/Model/CardsDetail.swift
@@ -0,0 +1,14 @@
+//
+//  CardsDetail.swift
+//  DeckBuilder
+//
+//  Created by an.chernyshev on 19/06/2019.
+//  Copyright © 2019 Anton Chernyshev. All rights reserved.
+//
+
+import Foundation
+
+struct CardsDetail: Codable {
+    var name: String?
+    var id: String?
+}
